30f42fb91c768e0c439143ada9b199edaf624bd0
akashkumar1691 <akashkumar1691@berkeley.edu>
2024-09-23 15:51:38 -0700

    farcaster bundling complete - minus verification

--- pkg/farcaster/go-grpc/util/bundle.go
+++ pkg/farcaster/go-grpc/util/bundle.go
@@ -2,9 +2,12 @@ package farcaster
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"strconv"
 
 	. "github.com/Hubmakerlabs/hoover/pkg"
+	ao "github.com/Hubmakerlabs/hoover/pkg/arweave"
 	"github.com/Hubmakerlabs/hoover/pkg/arweave/goar/types"
 	pb "github.com/juiceworks/hubble-grpc"
 )
@@ -35,6 +38,7 @@ func MessageToBundleItem(msg *pb.Message) (bundle *types.BundleItem, err error) 
 		return
 	}
 	bundle = &types.BundleItem{}
+	var data *ao.EventData
 	bundle.Tags = []types.Tag{
 		{Name: Protocol, Value: Farcaster},
 		{Name: Kind, Value: k},
@@ -45,10 +49,89 @@ func MessageToBundleItem(msg *pb.Message) (bundle *types.BundleItem, err error) 
 	}
 	switch k {
 	case Post:
+		data = ao.NewEventData(msg.GetData().GetCastAddBody().GetText())
+		embeds_deprecated := msg.GetData().GetCastAddBody().GetEmbedsDeprecated()
+		for i := range embeds_deprecated {
+			embed := embeds_deprecated[i]
+			ao.AppendTag(bundle, J(Embed, Uri), embed)
+		}
+		mentions := msg.GetData().GetCastAddBody().GetMentions()
+		if mentions != nil {
+			if len(mentions) == 1 {
+				data.Append(J(Mention), strconv.FormatUint(mentions[0], 10))
+			} else {
+				for i := range mentions {
+					mention := mentions[i]
+					data.Append(J(Mention, i), strconv.FormatUint(mention, 10))
+				}
+			}
+
+		}
+		parent := msg.GetData().GetCastAddBody().GetParent()
+		if parent != nil {
+			if x, ok := parent.(*pb.CastAddBody_ParentCastId); ok {
+				data.Append(J(Reply, Parent, Id), x.ParentCastId.String())
+			} else {
+				data.Append(J(Reply, Parent, Uri), parent.(*pb.CastAddBody_ParentUrl).ParentUrl)
+			}
+		}
+		mentions_positions := msg.GetData().GetCastAddBody().GetMentionsPositions()
+		if mentions_positions != nil {
+			if len(mentions_positions) == 1 {
+				data.Append(J(Mention, Start), strconv.FormatUint(uint64(mentions_positions[0]), 10))
+			} else {
+				for i := range mentions_positions {
+					mention_position := mentions_positions[i]
+					data.Append(J(Mention, i, Start), strconv.FormatUint(uint64(mention_position), 10))
+				}
+			}
+
+		}
+		embeds := msg.GetData().GetCastAddBody().GetEmbeds()
+		for i := range embeds {
+			if embeds[i].GetUrl() != "" {
+				data.Append(J(Embed, Uri), embeds[i].GetUrl())
+			} else {
+				data.Append(J(Embed, Id), embeds[i].GetCastId().String())
+			}
+		}
+
 	case Repost:
+		target := msg.GetData().GetReactionBody().GetTarget()
+		if x, ok := target.(*pb.ReactionBody_TargetCastId); ok {
+			target_id := x.TargetCastId.String()
+			data = ao.NewEventData(target_id)
+			ao.AppendTag(bundle, J(Repost, Event, Id), target_id)
+		} else {
+			target_url := target.(*pb.ReactionBody_TargetUrl).TargetUrl
+			data = ao.NewEventData(target_url)
+			ao.AppendTag(bundle, J(Repost, Event, Uri), target_url)
+		}
 	case Like:
+		target := msg.GetData().GetReactionBody().GetTarget()
+		if x, ok := target.(*pb.ReactionBody_TargetCastId); ok {
+			target_id := x.TargetCastId.String()
+			data = ao.NewEventData(target_id)
+			ao.AppendTag(bundle, J(Like, Event, Id), target_id)
+		} else {
+			target_url := target.(*pb.ReactionBody_TargetUrl).TargetUrl
+			data = ao.NewEventData(target_url)
+			ao.AppendTag(bundle, J(Like, Event, Uri), target_url)
+		}
+
 	case Follow:
+		follow_id := strconv.FormatUint(msg.GetData().GetLinkBody().GetTargetFid(), 10)
+		data = ao.NewEventData(follow_id)
+		ao.AppendTag(bundle, J(Follow, User, Id), follow_id)
 	case Profile:
+		data = ao.NewEventData(msg.GetData().GetUserDataBody().GetValue())
 	}
+	// put the ao.EventData into JSON form and place in the bundle.Data field
+	var b []byte
+	b, err = json.Marshal(data)
+	if err != nil {
+		return
+	}
+	bundle.Data = string(b)
 	return
 }
